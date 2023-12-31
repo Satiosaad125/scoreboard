be613141e6795ec37b0a6562c02612255a8b9631
Satiosaad125 <mohamed_165729@fci.sohag.edu.eg>
2023-12-31 04:35:18 +0200

    Modifying the whole project on github disktop

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,31 @@ import {defineConfig} from 'vite'
 export default defineConfig({
 	plugins: []	
 })
+let guestCounter = 0
+let homeCounter = 0
+let scoreGuest = document.getElementById("guest-score")
+let scoreHome = document.getElementById("home-score")
+function addGuestPoint1() {
+    
+    scoreGuest.textContent = guestCounter +=1
+    
+}
+function addHomePoint1(){
+    scoreHome.textContent = homeCounter +=1
+}
+function addGuestPoint2() {
+    scoreGuest.textContent = guestCounter +=2
+}
+function addHomePoint2() {
+    scoreHome.textContent = homeCounter+=2 
+}
+function addGuestPoint3() {
+    scoreGuest.textContent = guestCounter+=3
+}
+function addHomePoint3() {
+    scoreHome.textContent = homeCounter+=3
+}
+function newGame() {
+    scoreGuest.textContent = guestCounter =0
+    scoreHome.textContent = homeCounter = 0
+}
